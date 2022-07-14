4c1b37d10cc92f5350a2198b355d67605445df75
Francis Paolo <francispaolo.talan@gmail.com>
2022-07-15 04:30:39 +0800

    updated Firebase Auth

--- app/src/main/java/ph/cadet/cabote/talan/attendance/ChangePassword.kt
+++ app/src/main/java/ph/cadet/cabote/talan/attendance/ChangePassword.kt
@@ -2,10 +2,54 @@ package ph.cadet.cabote.talan.attendance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import ph.cadet.cabote.talan.attendance.databinding.ActivityChangePasswordBinding
+import ph.cadet.cabote.talan.attendance.databinding.ActivityLoginBinding
 
 class ChangePassword : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChangePasswordBinding
+
+    private lateinit var auth: FirebaseAuth
+    var currentPassword = binding.oldPasswordInput.toString()
+    var newPassword = binding.newPasswordInput.toString().trim()
+    var confirmNewPassword = binding.confirmNewPasswordInput.toString().trim()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+        binding = ActivityChangePasswordBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+
+        binding.changePasswordButton.setOnClickListener {
+            changePassword()
+        }
+    }
+
+    //FUNCTION-FOR-CHANGE-PASSWORD
+    private fun changePassword() {
+//        if (binding.oldPasswordInput.text.isNotEmpty() &&
+//            binding.newPasswordInput.text.isNotEmpty() &&
+//            binding.confirmNewPasswordInput.text.isNotEmpty()
+//        ) {
+//
+//            if (newPassword == confirmNewPassword) {
+//                Toast.makeText(
+//                    this@ChangePassword,
+//                    "Some Credentials Missing",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            } else {
+//                Toast.makeText(
+//                    this@ChangePassword,
+//                    "Some Credentials Missing",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//        }
     }
 }
